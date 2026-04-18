8f20d6623789a4e31cb4828382fb4b09786fb395
Swarup Ingale <swarupingale68@gmail.com>
2026-04-18 19:08:46 +0530

    Create exp02-3.js

---
+++ exp02/exp02-3.js
@@ -0,0 +1,21 @@
+// Calculator
+
+"use strict";
+function add(a, b) {
+    return a + b;
+}
+function sub(a, b) {
+    return a - b;
+}
+function mul(a, b) {
+    return a * b;
+}
+function div(a, b) {
+    return a / b;
+}
+let num1 = 10;
+let num2 = 20;
+console.log("addition : ", add(num1, num2));
+console.log("subtraction : ", sub(num1, num2));
+console.log("multiplication : ", mul(num1, num2));
+console.log("division : ", div(num1, num2));
